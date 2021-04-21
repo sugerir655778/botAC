97bda80a406138ccc78e678343b2495c9a2f50d5
sugerir655778 <djavandersonaraujo@gmail.com>
2021-04-21 00:37:47 -0500

    First commit

---
+++ src/menu.js
@@ -0,0 +1,40 @@
+const menu = (prefix) => {
+	return `
+
+            COMANDOS:
+
+
+  *ComandosBOT:*
+
+➸ *${prefix}loli*
+➸ *${prefix}hentai*
+➸ *${prefix}dono*
+➸ *${prefix}porno*
+➸ *${prefix}boanoite*
+➸ *${prefix}bomdia*
+➸ *${prefix}boatarde*
+➸ *${prefix}mia*
+➸ *${prefix}mia1*
+➸ *${prefix}mia2*
+➸ *${prefix}belle*
+➸ *${prefix}belle1*
+➸ *${prefix}belle2*
+➸ *${prefix}belle3*
+➸ *${prefix}ayeko*
+
+╔════════════════════
+  TRADUZIDO POR *WANDERSON *
+  DUVIDAS? 👇
+  WA.me/55979991632509
+╚════════════════════`
+}
+
+exports.menu = menu
+
+
+
+
+
+
+
+
